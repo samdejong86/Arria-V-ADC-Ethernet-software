04fc9f8e5fc6486b52777dba12d0e56a9c3c1991
Sam de Jong <samdejong86@gmail.com>
2018-05-18 10:48:07 -0700

    Added script to genereate ext_flash.flash

---
+++ scripts/elfFlash.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+quartusLoc=`which nios2-gdb-server`
+stringToRemove=/bin/nios2-gdb-server
+
+export QUARTUS_ROOTDIR=${quartusLoc%$stringToRemove}
+
+
+cd ../ADC_Socket
+elf2flash --input=ADC_Socket.elf --output=ext_flash.flash --base=0x0 --end=0x07FFFFFF --boot=/home/srdejong/intelFPGA/16.1/nios2eds/components/altera_nios2/boot_loader_cfi.srec
+cd $OLDPWD
